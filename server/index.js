335114aa4f30213da5147de448c0286c9468c65f
impetusalahurr1 <97399576+impetusalahurr1@users.noreply.github.com>
2022-04-11 19:55:22 +0200

    fully repair server side

--- server/index.js
+++ server/index.js
@@ -1,12 +1,27 @@
-const http = require('http');
+require('dotenv').config();
+const express=require('express')
+const cors=require('cors')
+const fileUpload=require('express-fileupload')
+const morgan=require('morgan')
 
 
+const auth = require('./routes/auth')
+const categ = require('./routes/categ')
 
-const server = http.createServer((req,res)=>{
-    
-})
+const app=express()
 
-server.listen(5000, 'localhost',()=>{
-    console.log('server listening on port 5000...')
-})
+app.use(cors())
+app.use(express.json)
+
+app.use(express.static("build"))
+
+app.use(fileUpload({useTempFiles:true}))
+app.use(morgan('dev'))
+
+app.use('/auth',auth)
+app.use('/categ',categ)
+
+const port= 5000
+
+app.listen(port,()=>console.log(`Server running on port ${port}`))
 
