b5279fa36172114ce4a91975c6cdc5d967e9aea2
Zsanber <zsanber@gmail.com>
2022-04-12 09:23:26 +0200

    index.js root

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,5 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-<<<<<<< Updated upstream
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />)
-=======
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
->>>>>>> Stashed changes
