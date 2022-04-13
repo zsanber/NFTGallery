d8d57c1ab8e76b741871185965481ba29c912f6c
Lili <nagy.lilian96@gmail.com>
2022-04-13 15:29:48 +0200

    Bitcoin

---
+++ client/src/components/BitcoinPrice.jsx
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react";
+import logo from "./bitcoin-logo.png";
+import "./BitcoinPrice.css";
+
+const BitcoinPrice = () => {
+    const [price, setPrice] = useState(null);
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+        fetch("https://blockchain.info/ticker")
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            setPrice(data.USD.last); 
+            setLoading(false);       
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }, []);
+      return (
+        <div className="btc">
+          <img className="btc-logo" src={logo} alt="Bitcoin" />
+          <span className="btc-price">
+            {loading ? "LOADING" : "$" + price}
+          </span>      
+        </div>
+      );
+  };
+  
+  export default BitcoinPrice;
