910fbabc8f015d76664091b0ef9c7ed6c92e4a96
Lili <nagy.lilian96@gmail.com>
2022-04-15 18:26:32 +0200

    login update, terms

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,14 +1,42 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 import { useForm } from 'react-hook-form';
+import {Terms} from './Terms';
 
 
-export const Login=()=> {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-      } = useForm();
-  const onSubmit = (data) => console.log(data);
+export const Login=({ setUser, setUserName, setUserId })=> {
+  const {register, handleSubmit, formState: { errors },} = useForm();
+  const [successful, setSuccessful] = useState(false);
+  const [msg, setMsg] = useState('');
+
+  useEffect(() => {
+    successful ? setUser(true) : setUser(false)
+  }, [successful])
+
+  const onSubmit = (data) =>{
+    console.log(data);
+    let url = '/auth/login'
+    sendData(url, data)
+  }
+
+  const sendData = async (url, formdata) => {
+    try {
+      const resp = await axios.post(url, formdata, { headers: { 'Content-Type': 'application/json' } })
+      const data = await resp.data
+      setSuccessful(true)
+      setUserName(data.username)
+      setUserId(data.userId)
+    } catch (err) {
+      console.log(err.message);
+      console.log(err.response.status)
+      if (err.response.status == 401) {
+        setSuccessful(false)
+        setUserName('')
+        setMsg(err.response.data.message)
+      } else
+        setMsg(err.message)
+    }
+  }
 
     return (
         <div className="reg">
@@ -24,8 +52,10 @@ export const Login=()=> {
                         <input type="submit" value="Login" className="btn btn-info form-control rounded mt-4 mb-1 fs-5 fw-bold text-white" />
                         <a className="text-info fw-bold text-decoration-none p-2" href="/Register">Not a member? Sign up Here!</a>
                     </form>
+                    <div>{msg}</div>
                 </div>
             </div>
+            <Terms />
         </div>
   )
 }
---
+++ client/src/components/Terms.jsx
@@ -0,0 +1,68 @@
+import React, {useEffect,useForm,useRef,useState} from 'react'
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
+
+export const Terms=()=>{
+    const [open, setOpen] = useState(false);
+    const [scroll, setScroll] = useState('paper');
+    
+    const handleClickOpen = (scrollType) => () => {
+        setOpen(true);
+        setScroll(scrollType);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const descriptionElementRef = useRef(null);
+    useEffect(() => {
+        if (open) {
+        const { current: descriptionElement } = descriptionElementRef;
+        if (descriptionElement !== null) {
+            descriptionElement.focus();
+        }
+        }
+    }, [open]);
+
+    return (
+        <div>
+          <Button color="secondary" alignitems="flex-end" style={{justifyContent: "flex-end"}} onClick={handleClickOpen('paper')}>Terms and Conditions</Button>
+          <Dialog
+            open={open}
+            onClose={handleClose}
+            scroll={scroll}
+            aria-labelledby="scroll-dialog-title"
+            aria-describedby="scroll-dialog-description"
+          >
+            <DialogTitle id="scroll-dialog-title">Terms and Conditions</DialogTitle>
+            <DialogContent dividers={scroll === 'paper'}>
+              <DialogContentText
+                id="scroll-dialog-description"
+                ref={descriptionElementRef}
+                tabIndex={-1}
+              >
+                {[...new Array(50)]
+                  .map(
+                    () => `Welcome to our website. If you continue to browse and use this website, you are agreeing to comply with and be bound by the following terms and conditions of use, which together with our privacy policy govern [business name]’s relationship with you in relation to this website. If you disagree with any part of these terms and conditions, please do not use our website.
+                    The term [business name] or us or we refers to the owner of the website whose registered office is [address]. Our company registration number is [company registration number and place of registration]. The term ‘you’ refers to the user or viewer of our website.
+                    The use of this website is subject to the following terms of use: •	The content of the pages of this website is for your general information and use only. It is subject to change without notice.
+                    •	This website uses cookies to monitor browsing preferences. If you do allow cookies to be used, the following personal information may be stored by us for use by third parties: [insert list of information].
+                    •	Neither we nor any third parties provide any warranty or guarantee as to the accuracy, timeliness, performance, completeness or suitability of the information and materials found or offered on this website for any particular purpose. You acknowledge that such information and materials may contain inaccuracies or errors and we expressly exclude liability for any such inaccuracies or errors to the fullest extent permitted by law.
+                    •	Your use of any information or materials on this website is entirely at your own risk, for which we shall not be liable. It shall be your own responsibility to ensure that any products, services or information available through this website meet your specific requirements.`,
+                  )
+                  .join('\n')}
+              </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleClose}>Close</Button>
+            </DialogActions>
+          </Dialog>
+        </div>
+      );
+    }
