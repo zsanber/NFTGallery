be5f1fa2f753cc0cc51ebdb5348f2ed72ae81683
Zsanber <zsanber@gmail.com>
2022-04-15 16:07:42 +0200

    searchbar

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -25,7 +25,7 @@ export const SearchBar=({pleceholder,posts})=> {
   return (
     <div>
         <div className="d-flex justify-content-between border rounded">
-            <input  className="border p-2" type="text" value={wordEntered} onChange={handleFilter}/>
+            <input  className="input p-1" type="text" value={wordEntered} onChange={handleFilter}/>
             {wordEntered===""? <i className="fa-solid fa-magnifying-glass"></i>:
                 <i className="fa-solid fa-x" onClick={handleClear}></i>}
         </div>
