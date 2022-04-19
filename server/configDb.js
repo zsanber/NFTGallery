89debda8fa413a273a7bc08151f7916ab5335c68
impetusalahurr1 <97399576+impetusalahurr1@users.noreply.github.com>
2022-04-19 17:10:16 +0200

    local server ready

--- server/configDb.js
+++ server/configDb.js
@@ -1,11 +1,17 @@
-require('dotenv').config();
-
 const configDb={
+    host     : process.env.MYSQL_HOST || 'localhost',
+    user     : process.env.MYSQL_USERNAME || 'root',
+    password : process.env.MYSQL_PASSWORD ||'',
+    database : process.env.MYSQL_DATABASE ||'nftgalleryDB',
+    multipleStatements :true
+}
+
+/*const configDb={
     host                : process.env.MYSQL_HOST ,
     user                : process.env.MYSQL_USERNAME,
     password            : process.env.MYSQL_PASSWORD,
     database            : process.env.MYSQL_DATABASE,
     multipleStatements  : true
-}
+}*/
 
 module.exports=configDb
