7b416707e6b277298a515035670752b8f5fdda70
Zsanber <zsanber@gmail.com>
2022-04-23 23:55:35 +0200

    sidebar (waiting for more renovation :)

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -8,16 +8,16 @@ export const Sidebar = ({userName}) => {
     <div className="homeBoxSideBar col-lg-3 col-sm-12 col-md-3 mt-5 mb-5 ml-20 order-2 justify-content-center align-items-center">
       <div className="sidebar rounded d-flex flex-column align-items-center">
         <img className="" src={myLogo} alt="AVATAR" />
-        <span className="welcome m-1 p-1 text-center w-75 fw-bold border-top border-bottom ">
+        <span className="welcome fw-bold border-top border-bottom ">
           Welcome {userName}
         </span>
 
         <BitcoinPrice />   
 
-        <div className="brands p-1 m-1" >
-          <a className="fa-brands fa-facebook fa-3x p-1 m-1 text-decoration-none" href="https://www.facebook.com"></a>
-          <a className="fa-brands fa-instagram fa-3x p-1 m-1 text-decoration-none" href="https://www.instagram.com"></a>
-          <a className="fa-brands fa-pinterest fa-3x p-1 m-1 text-decoration-none" href="https://www.pinterest.com"></a>
+        <div className="brands" >
+          <a className="fa-brands fa-facebook fa-2x p-1 m-1 text-decoration-none" href="https://www.facebook.com"></a>
+          <a className="fa-brands fa-instagram fa-2x p-1 m-1 text-decoration-none" href="https://www.instagram.com"></a>
+          <a className="fa-brands fa-pinterest fa-2x p-1 m-1 text-decoration-none" href="https://www.pinterest.com"></a>
         </div>
       </div>
     </div>
