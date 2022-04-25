af7cf01482d8bf12a90aea42ac12e5df2a4173f3
impetusalahurr1 <97399576+impetusalahurr1@users.noreply.github.com>
2022-04-25 21:39:46 +0200

    server config

--- server/configDb.js
+++ server/configDb.js
@@ -1,17 +1,17 @@
-const configDb={
+/*const configDb={
     host     : process.env.MYSQL_HOST || 'localhost',
     user     : process.env.MYSQL_USERNAME || 'root',
     password : process.env.MYSQL_PASSWORD || '',
     database : process.env.MYSQL_DATABASE ||'nftgallery',
     multipleStatements :true
-}
+}*/
 
-/*const configDb={
+const configDb={
     host                : process.env.MYSQL_HOST ,
     user                : process.env.MYSQL_USERNAME,
     password            : process.env.MYSQL_PASSWORD,
     database            : process.env.MYSQL_DATABASE,
     multipleStatements  : true
-}*/
+}
 
 module.exports=configDb
