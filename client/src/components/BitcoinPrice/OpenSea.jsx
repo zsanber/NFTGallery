0b14d45719c774a3aac3a56c4a968f0ada060de7
Lili <nagy.lilian96@gmail.com>
2022-04-29 01:30:39 +0200

    Edit test

--- client/src/components/BitcoinPrice/OpenSea.jsx
+++ client/src/components/BitcoinPrice/OpenSea.jsx
@@ -5,7 +5,7 @@ export const OpenSea = () => {
   return (
     <div className="btc">
       <img className="btc-logo" src="https://opensea.io/static/images/logos/opensea.svg" alt="Opensea" />
-      <Button variant="link" href="https://opensea.io/" >Opensea</Button>
+      <Button variant="link" href="https://opensea.io/" target="blank">Opensea</Button>
     </div>
   );
 };
--- client/src/components/Edit.jsx
+++ client/src/components/Edit.jsx
@@ -61,7 +61,7 @@ export const Edit = ({ userId, categoryList }) => {
 							<div className="col-md-6">
 								<select className="form-select m-2" {...register("categ_id", { required: true })} 
 										onChange={(event) => setPhotoCategory(event.target.value)}
-										value={photoCategory} >
+										value={photoCategory} data-testid="editComponent" >
 									{categoryList.map(obj => (
 										<option key={obj.idcategorie} value={obj.idcategorie} >
 											{obj.name}
